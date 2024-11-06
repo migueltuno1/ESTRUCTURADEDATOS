3838ae4b81dcfd4a82342d69fec4c436d91ac22a
MIGUEL TUNO LOAYZA <mtunol@est.emi.edu.bo>
2024-11-06 17:21:35 -0400

    creamos los contructores de paciente y medico

--- dominios/Medico.cs
+++ dominios/Medico.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace dominios
 {
-    internal class Medico
+    public class Medico
     {
+        private int idDoctor;
+        private string nombreDoctor;
+        private string apellidosDoctor;
+        private string cI_Doctor;
+        private string especialidad;
+
+        public Medico(int idDoctor, string nombreDoctor, string apellidosDoctor, string cI_Doctor, string especialidad)
+        {
+            this.idDoctor = idDoctor;
+            this.nombreDoctor = nombreDoctor;
+            this.apellidosDoctor = apellidosDoctor;
+            this.cI_Doctor = cI_Doctor;
+            this.especialidad = especialidad;
+        }
+
+        public int IdDoctor { get => idDoctor; set => idDoctor = value; }
+        public string NombreDoctor { get => nombreDoctor; set => nombreDoctor = value; }
+        public string ApellidosDoctor { get => apellidosDoctor; set => apellidosDoctor = value; }
+        public string CI_Doctor { get => cI_Doctor; set => cI_Doctor = value; }
+        public string Especialidad { get => especialidad; set => especialidad = value; }
+
+
+
+        // crear medicos
+
+        Medico doctor1 = new Medico(1, "Zora", "Loayza", "478952", "Medicina General");
+        Medico doctor2 = new Medico(2, "Leandro", "Tuno Loayza", "123456", "Odontologo");
+        Medico doctor3 = new Medico(3, "Maikol", "Rivero", "986354", "Traumatologia");
+
     }
 }
--- dominios/Paciente.cs
+++ dominios/Paciente.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace dominios
 {
-    internal class Paciente
+    public class Paciente
     {
+        private int idPaciente;
+        private String nombrePaciente;
+        private String apellidoPaciente;
+        private String cI_Paciente;
+        private DateTime fechaNac_Paciente;
+
+        public Paciente(int idPaciente, string nombrePaciente, string apellidoPaciente, string cI_Paciente, DateTime fechaNac_Paciente)
+        {
+            this.idPaciente = idPaciente;
+            this.nombrePaciente = nombrePaciente;
+            this.apellidoPaciente = apellidoPaciente;
+            this.cI_Paciente = cI_Paciente;
+            this.fechaNac_Paciente = fechaNac_Paciente;
+        }
+
+        public int IdPaciente { get => idPaciente; set => idPaciente = value; }
+        public string NombrePaciente { get => nombrePaciente; set => nombrePaciente = value; }
+        public string ApellidoPaciente { get => apellidoPaciente; set => apellidoPaciente = value; }
+        public string CI_Paciente { get => cI_Paciente; set => cI_Paciente = value; }
+        public DateTime FechaNac_Paciente { get => fechaNac_Paciente; set => fechaNac_Paciente = value; }
+
+
+
+
+
+
+        //creamos los pacientes directamente para hacer la prueba
+        Paciente paciente1 = new Paciente(1, "Miguel", "Tuno Loayza", "14205561", DateTime.Parse("17-06-2005"));
+        Paciente paciente2 = new Paciente(1, "Maximo", "Tuno Loayza", "14205560", DateTime.Parse("29-06-2005"));
+        Paciente paciente3 = new Paciente(1, "Emerson", "Cabral Moreno", "9478236", DateTime.Parse("19-02-2005"));
     }
 }
